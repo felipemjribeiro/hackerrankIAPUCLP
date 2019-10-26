dc108e025a15cfab0b24f713133ee93318b0eaa5
Felipe <felipemjribeiro@gmail.com>
2019-10-26 20:52:27 -0300

    exercicios
    exercicios

--- Arithmetic Operators.py
+++ Arithmetic Operators.py
@@ -1,6 +1,9 @@
+import Loops
 if __name__ == '__main__':
     a = int(input())
     b = int(input())
     print (a + b)
     print (a - b)
     print (a * b)
+    print (Loops.teste(a,b))
+
--- Loops.py
+++ Loops.py
@@ -2,3 +2,9 @@ if __name__ == '__main__':
     n = int(input())
     for i in range(0, n):
         print (i**2)
+
+
+def teste(a, b):
+    a = 1
+    b = 2
+    return a, b
