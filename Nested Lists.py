dbbd6e52bb5674d142c993e848e38b25cc09d2a9
Unknown <felipemjribeiro@gmail.com>
2019-10-27 17:58:25 -0300

    Dominguera
    com eletronica

--- Nested Lists.py
+++ Nested Lists.py
@@ -1,3 +1,4 @@
+from heapq import nsmallest
 if __name__ == '__main__':
     mainList = []
     for _ in range(int(input())):
@@ -5,6 +6,21 @@ if __name__ == '__main__':
         score = float(input())
         nameScore = list((name, score))
         mainList.append(nameScore)
-print(mainList)
+minValueVector = nsmallest(2, mainList, key=lambda e:e[1])
+secondValue = minValueVector[1][1]
+#print ('Segundo valor:' + str(secondValue))
+finalList = []
+fimWhile = len(mainList)
+while (fimWhile <= 0):
+    currentValue = mainList[fimWhile][1]
+    print ('Valor corrente:' + str(currentValue))
+    print ('Segundo valor:' + str(secondValue))
+    fimWhile -=1
+    if currentValue == secondValue:
+        print(mainList[fimWhile][0])
+'''
 for i in range(len(mainList)):
-    print(mainList[i][0])
+    currentValue = mainList[i][1]
+    #print ('Valor corrente:' + str(currentValue))
+    if currentValue == secondValue:
+        print(mainList[i][0])'''
